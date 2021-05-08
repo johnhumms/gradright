6a10dc8e17aa924541b9e5cafb0c44232f535cb6
Pikachu <jouxiong15@gmail.com>
2021-05-08 12:47:29 -0500

    finished basic student routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const districtRoutes = require('./districtRoutes')
+const districtRoutes = require('./districtRoutes');
+const studentRoutes = require('./studentRoutes')
 
 router.use('/user', userRoutes);
 router.use('/district', districtRoutes);
+router.use('/student', studentRoutes);
 
 module.exports = router;
