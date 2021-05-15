c28a90103d50572bc702e8c0d5bc7dfd048f41d4
Pikachu <jouxiong15@gmail.com>
2021-05-15 13:53:03 -0500

    fixed districts

--- public/js/edit-student.js
+++ public/js/edit-student.js
@@ -5,12 +5,12 @@ async function editStudent(event) {
     const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
     const student_name = document.querySelector('#stu-name').value;
-    const student_id = parseInt(document.querySelector('#stu-id').value);
+    const student_id = document.querySelector('#stu-id').value;
     const school_name = document.querySelector('#school').value;    
-    const current_year = parseInt(document.querySelector('#year').value);
-    const current_math_credits = parseInt(document.querySelector('#math').value);
+    const current_year = document.querySelector('#year').value;
+    const current_math_credits = document.querySelector('#math').value;
     // const picture = document.querySelector('#profile').value;
-    const district_id = parseInt(document.querySelector('#district').value);
+    const district_id = document.querySelector('#district').value;
 
       const response = await fetch(`/api/student/${id}`, {
         method: 'PUT',
